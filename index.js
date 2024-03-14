cf02876e9bc7b0ec3fb40268eca902b1606703f4
shourya <shourya>
2024-03-14 19:42:16 +0530

    ﻿ coomit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require("socket.io")(http, {
+  origin: "http://localhost:3000",
+});
+
+socketIO.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+    console.log(data);
+  });
+  console.log(`${socket.id} user connected`);
+  socket.on("disconnect", () => {
+    console.log("User disconected");
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
